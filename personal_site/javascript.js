1b034f5794a790d3520ba32f3cf4e55486a49585
Elhamudin Sameem <elhamsmacbookpro@Elhamudins-MacBook-Pro.local>
2023-04-17 14:40:51 -0400

    add: alert box for successful submission

--- personal_site/javascript.js
+++ personal_site/javascript.js
@@ -24,3 +24,27 @@ function scrollText() {
     // immediately call the function again
     requestAnimationFrame(scrollText);
 }
+
+
+// this code uses DOM manipulation and will alert the user of successfull submission.
+const form = document.querySelector("#form")
+const submitButton = document.querySelector("#submit")
+const scriptURL = 'https://httpbin.org/post';
+
+
+form.addEventListener('submit', e => {
+  submitButton.disabled = true
+  e.preventDefault()
+  let requestBody = new FormData(form)
+  fetch(scriptURL, { method: 'POST', body: requestBody})
+    .then(response => {
+       alert('Success!', response)
+       submitButton.disabled = false
+      })
+    .catch(error => {
+    alert('Error!', error.message)
+      submitButton.disabled = false
+
+    }
+    )
+})
